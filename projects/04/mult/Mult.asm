3160a5ebb98099d20e0ddb149458eb80de6ad6db
highwide <hochweit728@gmail.com>
2022-08-10 10:55:17 +0900

    Implement Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// r2 = r0 * r1
+//
+// r2=0
+// for (i = 0, i++, i < r1) {
+//   r2 += r0
+// }
+
+    @i
+    M=0
+    @R2
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R1
+    D=M-D
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
